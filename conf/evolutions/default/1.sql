d10726f8f1d0cb47ae6a4b7f4094e000e6b51216
mnceleiro <markiusarria@gmail.com>
2017-04-24 09:49:26 +0200

    Arreglado fallo en el script de creación de BBDD

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -15,23 +15,26 @@ CREATE TABLE USER (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ROLES (
+CREATE TABLE ROLE (
   id bigint(20) NOT NULL,
-  description varchar(100) NOT NULL
+  description varchar(100) NOT NULL,
+  PRIMARY KEY (id)
 );
-
-INSERT INTO ROLES (id, description) VALUES (1, "IP");
-INSERT INTO ROLES (id, description) VALUES (2, "PROFESSOR");
-INSERT INTO ROLES (id, description) VALUES (3, "POSTDOC");
-INSERT INTO ROLES (id, description) VALUES (4, "DOC");
-INSERT INTO ROLES (id, description) VALUES (5, "SCHOLAR");
-INSERT INTO ROLES (id, description) VALUES (6, "OTHERS");
 
 INSERT INTO USER (email, password, first_name, last_name, signature_name, address, phone, role) VALUES ('mnceleiro@esei.uvigo.es','$2a$12$l/xZ9uxuWijTvc14Ff5AVur6FIFMogs0DFdVhlfmHR3XWgvmQIVke','Marcos','Nunez Celeiro', 'Marcos Nunez Celeiro', 'Calle Empanada de Zorza nº5', '9825312121', 2);
 INSERT INTO USER (email, password, first_name, last_name, signature_name, address, phone, role) VALUES ('ocanellas@gmail.com','1234','Oscar','Cañellas', 'Oscar Mixwell Cañellas', 'Calle Alcantara nº12', '9825312122', 2);
 INSERT INTO USER (email, password, first_name, last_name, signature_name, address, phone, role) VALUES ('dmritchie@yahoo.es','1234','Dennis','MacAlistair Ritchie', 'Dennis MacAlistair-Ritchie', 'Avenida Curros Enriquez 7', '9825312123', 2);
 INSERT INTO USER (email, password, first_name, last_name, signature_name, address, phone, role) VALUES ('ltorval@esei.uvigo.es','1234','Linus','Benedict Torvalds', 'Linus Benedict-Torvalds', 'Plaza Pontevedra 122', '9825312124', 2);
 INSERT INTO USER (email, password, first_name, last_name, signature_name, address, phone, role) VALUES ('mrjato@esei.uvigo.es','1234','Miguel','Reboiro Jato', 'Miguel Reboiro-Jato', 'Avenida de la Coruña 17', '9825312125', 2);
+
+
+INSERT INTO ROLE (id, description) VALUES (1, 'IP');
+INSERT INTO ROLE (id, description) VALUES (2, 'PROFESSOR');
+INSERT INTO ROLE (id, description) VALUES (3, 'POSTDOC');
+INSERT INTO ROLE (id, description) VALUES (4, 'DOC');
+INSERT INTO ROLE (id, description) VALUES (5, 'SCHOLAR');
+INSERT INTO ROLE (id, description) VALUES (6, 'OTHERS');
+
 
 # --- !Downs
 
