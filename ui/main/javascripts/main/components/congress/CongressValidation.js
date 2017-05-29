2067893a804650f4397c699a7d084a14a438d851
mnceleiro <markiusarria@gmail.com>
2017-05-29 16:23:52 +0200

    - Validación de fechas en detalle de congreso.

--- ui/main/javascripts/main/components/congress/CongressValidation.js
+++ ui/main/javascripts/main/components/congress/CongressValidation.js
@@ -19,5 +19,20 @@ export const validate = formProps => {
     errors.country = "Por favor, introduzca un país."
   }
 
+  if (formProps.startDate && !DateUtils.isValid(formProps.startDate)) {
+    errors.startDate = "Por favor, introduzca una fecha valida."
+  }
+
+  if (formProps.endDate && !DateUtils.isValid(formProps.endDate)) {
+    errors.endDate = "La fecha introducida no es válida."
+  }
+
+  if (formProps.startDate && formProps.endDate) {
+    if (DateUtils.greaterThan(formProps.startDate, formProps.endDate)) {
+      errors.startDate = "La fecha de inicio no puede ser superior a la de fin."
+      errors.endDate = "La fecha de inicio no puede ser superior a la de fin."
+    }
+  }
+
   return errors
 }
